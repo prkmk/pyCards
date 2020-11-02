970deb2089e1c234b0a26bdbb8745bc041c0c5b5
Lauri Parkkamäki <lauri.parkkamaki@tuni.fi>
2020-11-02 10:07:23 +0200

    Card implementation

---
+++ card.py
@@ -0,0 +1,33 @@
+import enum
+from colorama import init, Fore, Back, Style
+
+class Suits(enum.Enum):
+  HEART = "♥"
+  SPADE = "♠"
+  DIAMOND = "♦"
+  CLUB = "♣"
+
+class Card:
+  def __init__(self, suit = Suits.HEART, value = 1, revealed = True):
+    self.value = 14 if value == 1 else value
+    self.suit = suit
+    self.revealed = revealed
+  
+  def print(self, end = "\n"):
+    if self.revealed:
+      f = Fore.RED if (self.suit == Suits.HEART or self.suit == Suits.DIAMOND) else Fore.BLACK
+      b = Back.WHITE
+      v = "J" if self.value == 11 else "Q" if self.value == 12 else "K" if self.value == 13 else "A" if self.value == 14 else self.value
+      s = self.suit.value + "{:>2}".format(v)
+      print(f + b + s + Style.RESET_ALL, end=end)
+    else:
+      print(Back.GREEN + "   " + Style.RESET_ALL, end=end)
+    
+  def reveal(self):
+    self.revealed = True
+  
+  def hide(self):
+    self.revealed = False
+  
+  def flip(self):
+    self.revealed = not self.revealed
